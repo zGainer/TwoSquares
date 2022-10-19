8d381c69c731c28955aec6496e688825853490ef
Eugene <zgainer@gmail.com>
2022-10-19 20:30:00 +0300

    Initial Commit

---
+++ TwoSquares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSquares
+//
+//  Created by Eugene on 19.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
